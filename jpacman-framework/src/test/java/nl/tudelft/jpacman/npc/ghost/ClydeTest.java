1375fbb25e505df2111bc9e6e53ac5e1619a09dc
Ska02 <114934118+Ska02@users.noreply.github.com>
2025-10-21 08:54:37 +0200

    update TP2 exercices

--- jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ jpacman-framework/src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -12,10 +12,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClydeTest {
-    private PacManSprites pacManSprites = new PacManSprites();
+
+    /*private PacManSprites pacManSprites = new PacManSprites();
     private PlayerFactory playerFactory = new PlayerFactory(pacManSprites);
     private GhostFactory ghostFactory = new GhostFactory(pacManSprites);
     private LevelFactory levelFactory = new LevelFactory(pacManSprites, ghostFactory);
@@ -38,5 +40,180 @@ class ClydeTest {
         assertNotNull(clyde);
         Optional<Direction> direction = clyde.nextAiMove();
         assertEquals(Optional.empty(), direction);
+    }*/
+
+    private GhostMapParser parser;
+    private PlayerFactory playerFactory;
+
+    @BeforeEach
+    void setUp() {
+        PacManSprites sprites = new PacManSprites();
+        BoardFactory boardFactory = new BoardFactory(sprites);
+        GhostFactory ghostFactory = new GhostFactory(sprites);
+        LevelFactory levelFactory = new LevelFactory(sprites, ghostFactory);
+
+        parser = new GhostMapParser(levelFactory, boardFactory, ghostFactory);
+        playerFactory = new PlayerFactory(sprites);
+    }
+
+    @Test
+    void smallDistancePathFree() {
+        String[] map = {
+            "############",
+            "#P    C    #",
+            "############"
+        };
+
+        Level level = parser.parseMap(List.of(map));
+
+        Player player = playerFactory.createPacMan();
+        level.registerPlayer(player);
+
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
+        assertThat(clyde).isNotNull();
+
+        Optional<Direction> nextMove = clyde.nextAiMove();
+
+        assertThat(nextMove).isPresent();
+        assertThat(nextMove.get()).isEqualTo(Direction.EAST);
+    }
+
+
+    @Test
+    void smallDistancePathKO() {
+        String[] map = {
+            "############",
+            "#P    #C#  #",
+            "############"
+        };
+
+        Level level = parser.parseMap(List.of(map));
+
+        Player player = playerFactory.createPacMan();
+        level.registerPlayer(player);
+
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
+        assertThat(clyde).isNotNull();
+
+        Optional<Direction> nextMove = clyde.nextAiMove();
+
+        assertThat(nextMove).isEmpty();
+    }
+
+
+    @Test
+    void bigDistancePathFree() {
+        String[] map = {
+            "############",
+            "#P         #",
+            "            ",
+            "            ",
+            "            ",
+            "            ",
+            "            ",
+            "            ",
+            "           C",
+            "############"
+        };
+
+        Level level = parser.parseMap(List.of(map));
+
+        Player player = playerFactory.createPacMan();
+        level.registerPlayer(player);
+
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
+        assertThat(clyde).isNotNull();
+
+        Optional<Direction> nextMove = clyde.nextAiMove();
+
+        assertThat(nextMove).isPresent();
+        assertThat(nextMove.get()).isEqualTo(Direction.NORTH);
+    }
+
+
+    @Test
+    void bigDistancePathKO() {
+        String[] map = {
+            "############",
+            "#P         #",
+            "            ",
+            "            ",
+            "            ",
+            "            ",
+            "            ",
+            "         ###",
+            "         #C#",
+            "############"
+        };
+
+        Level level = parser.parseMap(List.of(map));
+
+        Player player = playerFactory.createPacMan();
+        level.registerPlayer(player);
+
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
+        assertThat(clyde).isNotNull();
+
+        Optional<Direction> nextMove = clyde.nextAiMove();
+
+        assertThat(nextMove).isEmpty();
+    }
+
+    @Test
+    void multiplePacman() {
+        String[] map = {
+            "############",
+            "#P         #",
+            "            ",
+            "            ",
+            "    P       ",
+            "            ",
+            "            ",
+            "            ",
+            "     P     C",
+            "############"
+        };
+
+        Level level = parser.parseMap(List.of(map));
+
+        Player player = playerFactory.createPacMan();
+        level.registerPlayer(player);
+
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
+        assertThat(clyde).isNotNull();
+
+        Optional<Direction> nextMove = clyde.nextAiMove();
+
+        assertThat(nextMove).isPresent();
+        assertThat(nextMove.get()).isEqualTo(Direction.NORTH);
+    }
+
+    @Test
+    void multipleClyde() {
+        String[] map = {
+            "############",
+            "#P         #",
+            "            ",
+            " C          ",
+            "        C   ",
+            "            ",
+            "            ",
+            "    C       ",
+            "           C",
+            "############"
+        };
+
+        Level level = parser.parseMap(List.of(map));
+
+        Player player = playerFactory.createPacMan();
+        level.registerPlayer(player);
+
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
+        assertThat(clyde).isNotNull();
+
+        Optional<Direction> nextMove = clyde.nextAiMove();
+
+        assertThat(nextMove).isPresent();
+        assertThat(nextMove.get()).isEqualTo(Direction.SOUTH);
     }
 }
